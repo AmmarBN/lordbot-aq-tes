24bcf0f011c28a5baf497fe6575068b28d9a967b
Lord_Ammar <83591869+AmmarBN@users.noreply.github.com>
2021-12-29 16:14:59 +0000

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,16 @@
+let handler = async m => m.reply(`
+╭─「 Donasi • Pulsa 」
+│ • XL [087708773367]
+│ • Telkomsel [082136981440]
+╰────
+
+╭─「 Donasi • Non Pulsa 」
+│ • https://saweria.co/Nurutomo
+│ • https://saweria.co/ariffb
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
